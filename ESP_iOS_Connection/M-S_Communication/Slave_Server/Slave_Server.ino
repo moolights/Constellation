4e802b1ed6ef5a23bc70a7a5dfdb910b937bc5c3
Bryton Lee <brl013@ucsd.edu>
2024-05-01 23:29:29 -0700

    characteristics

--- ESP_iOS_Connection/M-S_Communication/Slave_Server/Slave_Server.ino
+++ ESP_iOS_Connection/M-S_Communication/Slave_Server/Slave_Server.ino
@@ -13,21 +13,23 @@
 // Initialize all pointers
 BLEServer* pServer = NULL;                        // Pointer to the server
 BLECharacteristic* pCharacteristic_1 = NULL;      // Pointer to Characteristic 1
+BLECharacteristic* pCharacteristic_2 = NULL;      // Pointer to Characteristic 2
+BLECharacteristic* pCharacteristic_3 = NULL;      // Pointer to Characteristic 3
 BLE2902 *pBLE2902_1;                              // Pointer to BLE2902 of Characteristic 1
+BLE2902 *pBLE2902_2;                              // Pointer to BLE2902 of Characteristic 2
+BLE2902 *pBLE2902_3;                              // Pointer to BLE2902 of Characteristic 3
 
 // Some variables to keep track on device connected
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-
-// Variable that will continuously be increased and written to the client
-uint32_t value = 0;
-uint16_t dc_counter = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 // UUIDs used in this example:
 #define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID_1 "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID_2 "4d3f12b8-78d2-48c2-8997-da30fbd08c26"
+#define CHARACTERISTIC_UUID_3 "114c4080-cae6-42c3-bc89-35e465c41219"
 
 // Callback function that is called whenever a client is connected or disconnected
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -74,16 +76,37 @@ void setup() {
 
   // Create a BLE Characteristic
   pCharacteristic_1 = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID_1,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |                      
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );  
-
-  // Create a BLE Descriptor  
+                        CHARACTERISTIC_UUID_1,
+                        BLECharacteristic::PROPERTY_READ   |
+                        BLECharacteristic::PROPERTY_WRITE  |
+                        BLECharacteristic::PROPERTY_NOTIFY
+                      );
+                      
+  pCharacteristic_2 = pService->createCharacteristic(
+                        CHARACTERISTIC_UUID_2,
+                        BLECharacteristic::PROPERTY_READ   |
+                        BLECharacteristic::PROPERTY_WRITE  |
+                        BLECharacteristic::PROPERTY_NOTIFY
+                      );
+                      
+  pCharacteristic_3 = pService->createCharacteristic(
+                        CHARACTERISTIC_UUID_3,
+                        BLECharacteristic::PROPERTY_READ   |
+                        BLECharacteristic::PROPERTY_WRITE  |
+                        BLECharacteristic::PROPERTY_NOTIFY
+                      );
+  // Create a BLE Descriptor
   pBLE2902_1 = new BLE2902();
   pBLE2902_1->setNotifications(true);
   pCharacteristic_1->addDescriptor(pBLE2902_1);
+
+  pBLE2902_2 = new BLE2902();
+  pBLE2902_2->setNotifications(true);
+  pCharacteristic_2->addDescriptor(pBLE2902_2);
+
+  pBLE2902_3 = new BLE2902();
+  pBLE2902_3->setNotifications(true);
+  pCharacteristic_3->addDescriptor(pBLE2902_3);
 
   // Start the service
   pService->start();
@@ -99,14 +122,20 @@ void setup() {
 
 void loop() {
   checkToReconnect();
-    // notify changed value
-    if (deviceConnected) {
-      std::string value = pCharacteristic_1->getValue();
-      if (value == "ON") {
-        digitalWrite(LED_PIN, HIGH);
-      } else {
-        digitalWrite(LED_PIN, LOW);
-      }
-      delay(1000);
+  // notify changed value
+  if (deviceConnected) {
+    std::string value_1 = pCharacteristic_1->getValue();
+    std::string value_2 = pCharacteristic_2->getValue();
+    std::string value_3 = pCharacteristic_3->getValue();
+    if (value_1 == "ON") {
+      digitalWrite(LED_PIN, HIGH);
+    } else {
+      digitalWrite(LED_PIN, LOW);
     }
+    Serial.print("Value 2: ");
+    Serial.println(value_2.c_str());
+    Serial.print("Value 3: ");
+    Serial.println(value_3.c_str());
+    delay(1000);
+  }
 }
